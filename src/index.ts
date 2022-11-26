5b9671292dd676b4d8955b62bfc2d4572c067a85
Kevin Vega <kevinvega2070@gmail.com>
2022-11-25 22:39:36 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { Client, LocalAuth } from "whatsapp-web.js";
+import qrcode from 'qrcode-terminal';
+
+const client = new Client({
+  authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+client.on('message', (message) => {
+	console.log(message.body);
+  client.sendMessage(message.from, message.body);
+});
+
+client.initialize();
